8c3715c597c9d6965646f9d3e5111f46cf688ada
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-25 07:57:38 +0000

    authorize comment controller with pundit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,21 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
   before_action :is_an_authorized_user, only: [:create]
-  before_action :authorize_comment_author, only: [:edit, :update, :destroy]
+  before_action :ensure_current_user_is_author, only: [:edit, :update, :destroy]
 
   # GET /comments or /comments.json
   def index
-    @comments = Comment.all
+  @comments = policy_scope(Comment)
   end
 
   # GET /comments/1 or /comments/1.json
   def show
+    authorize @comment
   end
 
   # GET /comments/new
   def new
+    authorize @comment
     @comment = Comment.new
   end
 
@@ -25,6 +27,8 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.author = current_user
+
+    authorize @comment
 
     respond_to do |format|
       if @comment.save
@@ -39,6 +43,7 @@ class CommentsController < ApplicationController
 
   # PATCH/PUT /comments/1 or /comments/1.json
   def update
+    authorize @comment
     respond_to do |format|
       if @comment.update(comment_params)
         format.html { redirect_to root_url, notice: "Comment was successfully updated." }
@@ -52,6 +57,7 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1 or /comments/1.json
   def destroy
+    authorize @comment
     @comment.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
@@ -60,28 +66,28 @@ class CommentsController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  def is_an_authorized_user
-    @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
-    
-    if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
-      redirect_back fallback_location: root_url, alert: "Not authorized"
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
-  end
 
-  def authorize_comment_author
-    unless current_user == @comment.author
-      redirect_back fallback_location: root_url, alert: "You're not authorized for that"
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:author_id, :photo_id, :body)
     end
-  end
 
-  # Only allow a list of trusted parameters through.
-  def comment_params
-    params.require(:comment).permit(:author_id, :photo_id, :body)
-  end
+    def is_an_authorized_user
+      @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
+      if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that. From Comments Controller."
+      end
+    end
+
+  
+    def ensure_current_user_is_author
+      if @comment.author != current_user
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that. From Comments Controller."
+      end
+    end
+
 end
