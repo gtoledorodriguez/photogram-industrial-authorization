b87d689b3b07ea4f046784e29633aec522de232e
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-25 04:53:44 +0000

    added except: to limit photos routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :follow_requests, except: [:index, :show, :new,:edit]
   resources :likes, only: [:create, :destroy]
-  resources :photos
+  resources :photos, except: [:index]
   resources :users, only: [ :index ]
 
   get ":username" => "users#show", as: :user
