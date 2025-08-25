c3d00e92ba5e166756675571bf6b4f7da3931423
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-25 06:31:48 +0000

    added authorization so user can only look at their own feed and discover pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed discover ]
+  before_action :authorize_feed_and_discover_access, only: [:feed, :discover]
 
   def index
     @users = @q.result
@@ -14,4 +15,10 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def authorize_feed_and_discover_access
+    if current_user != @user
+      redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+    end
+  end
 end
