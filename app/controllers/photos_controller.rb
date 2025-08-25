ab2a35b7eaaaecb6684c8c142a4fb7246dd5bb5d
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-25 07:56:13 +0000

    authorized photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,24 +1,37 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
-  before_action :ensure_current_user_is_owner, only: %i[ edit update destroy ]
+  before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
+  # before_action :ensure_user_is_authorized, only: [:show]
+
+  # GET /photos or /photos.json
+  def index
+    @photos = policy_scope(Photo)
+  end
 
   # GET /photos/1 or /photos/1.json
   def show
+    authorize @photo
   end
 
   # GET /photos/new
   def new
     @photo = Photo.new
+    @photo.owner = current_user
+    authorize @photo
+    return
   end
 
   # GET /photos/1/edit
   def edit
+    authorize @photo
   end
 
   # POST /photos or /photos.json
   def create
     @photo = Photo.new(photo_params)
     @photo.owner = current_user
+
+    authorize @photo
 
     respond_to do |format|
       if @photo.save
@@ -33,6 +46,8 @@ class PhotosController < ApplicationController
 
   # PATCH/PUT /photos/1 or /photos/1.json
   def update
+
+    authorize @photo
     respond_to do |format|
       if @photo.update(photo_params)
         format.html { redirect_to @photo, notice: "Photo was successfully updated." }
@@ -46,6 +61,8 @@ class PhotosController < ApplicationController
 
   # DELETE /photos/1 or /photos/1.json
   def destroy
+
+    authorize @photo
     @photo.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Photo was successfully destroyed." }
@@ -61,9 +78,16 @@ class PhotosController < ApplicationController
 
     def ensure_current_user_is_owner
       if current_user != @photo.owner
-        redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that. From Photos Controller."
       end
     end
+
+    # def ensure_user_is_authorized
+    #   if !PhotoPolicy.new(current_user, @photo).show?
+    #     # redirect_back fallback_location: root_url
+    #     raise Pundit::NotAuthorizedError, "not allowed"
+    #   end
+    # end
 
     # Only allow a list of trusted parameters through.
     def photo_params
