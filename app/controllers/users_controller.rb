e03ecfb020ba03a623ad6183a48d4eef4b66a40e
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-25 07:55:05 +0000

    updated user_controller to authorize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed discover ]
-
-  before_action :must_be_owner_to_view, only: %i[ feed discover ]
+  before_action :is_an_authorized_user, only: [:feed, :discover]
 
   def index
     @users = @q.result
+  end
+
+
+  def feed
+    authorize @user
+  end
+
+  def discover
+    authorize @user
+  end
+
+  def liked
+    authorize @user
+  end
+
+  def show
+    authorize @user
   end
 
   private
@@ -17,9 +33,9 @@ class UsersController < ApplicationController
       end
     end
 
-    def must_be_owner_to_view
-      if current_user != @user
-        redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+    def is_an_authorized_user
+      if @user != current_user
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that. From Users Controller."
       end
     end
 end
