437518015078afdbfcbe2b117214b5384a921bbe
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-25 07:25:46 +0000

    implement more permissive show? policy  in user_policy; generated ApplicationPolicy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -7,9 +7,13 @@ class UserPolicy
   end
 
   def show?
+    true
+  end
+
+  def show_photos?
     user == current_user ||
-      !user.private? ||
-      user.followers.include?(current_user)
+     !user.private? || 
+     user.followers.include?(current_user)
   end
 
   def feed?
